0943f00f4ed07262098bd2c880f83fee2cec8256
Alex Aloi <s3842524@student.rmit.edu.au>
2025-06-25 19:28:06 +1000

    changed database to mysql

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -4,18 +4,21 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 export const getDatabaseConfig = (
   configService: ConfigService,
 ): TypeOrmModuleOptions => {
-  // Option 2: Using individual environment variables
   return {
-    type: 'postgres',
+    type: 'mysql',
     host: configService.get('DB_HOST'),
     port: parseInt(configService.get('DB_PORT')!),
     username: configService.get('DB_USERNAME'),
     password: configService.get('DB_PASSWORD'),
     database: configService.get('DB_DATABASE'),
     entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-    synchronize: true, // Set to false in production
-    ssl: {
-      rejectUnauthorized: false,
+    synchronize: false, // Important: Set to false for production databases
+    ssl: false,
+    extra: {
+      connectionLimit: 10, // Limit connections for serverless
     },
+    supportBigNumbers: true,
+    bigNumberStrings: false,
+    dateStrings: false,
   };
 };
